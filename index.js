160687e9f82e33c94bc2f699b3a8f20c4e1932e2
gld <gld@encode.dk>
2017-06-27 11:28:54 +0300

    faefawfaw

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const requestLib = require('request');
+var mcache = require('memory-cache');
+
+var cache = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.url
+        let cachedBody = mcache.get(key)
+        if (cachedBody) {
+            console.log('cachedBody');
+            res.send(cachedBody)
+            return
+        } else {
+            res.sendResponse = res.send
+            res.send = (body) => {
+                mcache.put(key, body, duration * 1000);
+                res.sendResponse(body)
+            }
+            next()
+        }
+    }
+}
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/give-me-page', cache(10), function (request, response) {
+    console.log('do request');
+    requestLib.get(request.query.page, {}, (err, res, body) => {
+        if (err) {
+            console.log(err);
+        }
+        if (res.statusCode === 200) {
+            response.send(body)
+        }
+    });
 });
 
 
